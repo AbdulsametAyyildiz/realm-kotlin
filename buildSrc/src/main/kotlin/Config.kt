2df6caf1813391d4f32f9a02658ae315eb2d45f6
Abdülsamet Ayyıldız <abdussamet.ayyildiz@gmail.com>
2026-05-01 18:05:53 +0300

    Bump AGP to 8.13.2 (max 8.x for Kotlin 2.3) and clean buildSrc deps
    - Config.kt: buildTools 8.7.3 -> 8.13.2 (latest AGP 8.x patch)
    - Config.kt: r8 8.7.18 -> 8.13.19 (matches AGP 8.13.x for Kotlin 2.3)
    - buildSrc: remove kotlin-compiler-embeddable (warned by Kotlin Gradle
      plugin: artifact present in build classpath alongside KGP causes
      unpredictable behavior)
    - buildSrc: remove duplicate kotlin-gradle-plugin (already pulled by
      kotlin("gradle-plugin", version = ...))
    - buildSrc: remove gradle-kotlin-dsl-plugins:5.2.0 (bundled into
      kotlin-dsl since Gradle 6.0)
    
    Note: AGP 9.x requires migrating to com.android.kotlin.multiplatform.library,
    which does not yet support externalNativeBuild/CMake/NDK. Realm-core
    needs that, so we stay on the latest AGP 8.x line until Google adds
    NDK support to the new KMP plugin.

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -101,9 +101,9 @@ object Versions {
         const val targetSdk = 35
         const val compileSdkVersion = 35
         const val buildToolsVersion = "35.0.0"
-        const val buildTools = "8.7.3" // https://maven.google.com/web/index.html?q=gradle#com.android.tools.build:gradle
+        const val buildTools = "8.13.2" // https://maven.google.com/web/index.html?q=gradle#com.android.tools.build:gradle
         const val ndkVersion = "27.0.12077973"
-        const val r8 = "8.7.18" // See https://developer.android.com/build/kotlin-support
+        const val r8 = "8.13.19" // See https://developer.android.com/build/kotlin-support
     }
     const val androidxBenchmarkPlugin = "1.3.3" // https://maven.google.com/web/index.html#androidx.benchmark:androidx.benchmark.gradle.plugin
     const val androidxStartup = "1.2.0" // https://maven.google.com/web/index.html?q=startup#androidx.startup:startup-runtime
